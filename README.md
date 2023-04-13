ecffe91a332f8cfbdbe4263a91c20e7f39092776
devres <potolinresty110394@gmail.com>
2023-04-13 08:57:47 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# glassmorpcard
---
+++ dist/script.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+VanillaTilt.init(document.querySelectorAll(".box"), {
+  max: 25,
+  speed: 400,
+  easing: "cubic-bezier(.03,.98,.52,.99)",
+  perspective: 500,
+  transition: true
+});
+//# sourceMappingURL=script.dev.js.map
